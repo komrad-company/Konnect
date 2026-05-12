d3849095ca3103118207aa948e8065ca6637c14c
wampixel <wampixel@gmail.com>
2026-05-13 00:56:58 +0200

    [FIX] Store trait

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,11 @@
+use std::future::Future;
+
 use sqlx::PgPool;
 
-pub trait Store {
+use crate::errors::Error;
+
+pub trait Store: Sized {
+    fn new(pool: PgPool) -> Self;
     fn pool(&self) -> &PgPool;
+    fn migrate(&self) -> impl Future<Output = Result<(), Error>> + Send + '_;
 }
